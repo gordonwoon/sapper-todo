cdb5d5bf6d4420fc305a5931edc90c7939d05ced
GordonWoon <gordonwoon@live.com>
2020-10-26 10:15:53 +0800

    init

---
+++ src/routes/story/_Comment.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let comment
+</script>
+
+<div class="comment">
+  <div class="details">
+
+    <div class="voter">
+      <span class="upvoter">
+        <span class="arrow"></span>
+      </span>
+      <span class="score">{comment.score}</span>
+    </div>
+
+    <div class="main">
+      <div class="byline">
+        <span class="author">{comment.user.username}</span>
+        <span class="date">{comment.createdAt}</span>
+      </div>
+      <p class="comment-content">{comment.content}</p>
+    </div>
+  </div>
+  {#if comment.replies && comment.replies.length}
+    <div class="replies">
+      {#each comment.replies as reply (reply._id)}
+        <svelte:self
+          comment="{reply}"
+        />
+      {/each}
+    </div>
+  {/if}
+</div>
---
+++ src/routes/story/_CommentContainer.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Comment from './_Comment.svelte'
+  let comments = [
+    {
+      score: 0,
+      _id: '1',
+      content: 'first comment',
+      user: {
+        username: 'test user',
+      },
+      createdAt: '5 minutes ago',
+      replies: []
+    },
+    {
+      score: 0,
+      _id: '2',
+      content: 'second comment',
+      user: {
+        username: 'test user 2',
+      },
+      createdAt: '3 minutes ago',
+      replies: [
+        {
+          score: 0,
+          _id: '3',
+          content: 'first reply',
+          user: {
+            username: 'test user',
+          },
+          createdAt: '2 minutes ago',
+          replies: []
+        }
+      ]
+    }
+  ]
+</script>
+
+<div class="comment-list">
+  {#each comments as comment (comment._id)}
+    <Comment
+      {comment}
+    />
+  {/each}
+</div>
